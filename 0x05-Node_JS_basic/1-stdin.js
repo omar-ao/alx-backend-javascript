98d377af525e2832c710a0a44a2afdc040166d05
Omar Ahmed O <deeyndiid@gmail.com>
2024-12-01 09:24:24 +0000

    Add task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-
   process.stdout.write(`Your name is: ${name}\n`);
 
-  process.stdin.end();
+  if (process.stdin.isTTY) {
+    process.exit(0);
+  }
 });
 
 process.stdin.on('end', () => {
