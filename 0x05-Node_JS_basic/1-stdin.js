287a4c11e3cfdee9e6492df7fe824abe314ce442
Omar Ahmed O <deeyndiid@gmail.com>
2024-12-01 09:24:24 +0000

    Add task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-
   process.stdout.write(`Your name is: ${name}\n`);
 
-  process.stdin.end();
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  } else {
+    process.exit();
+  }
 });
